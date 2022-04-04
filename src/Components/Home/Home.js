9cdfda93c15f2cdbdf9d02598859236212bda359
SadikHasanRafi <sadikkhasan@gmail.com>
2022-04-04 16:25:21 +0600

    404 page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,11 +3,11 @@ import Img from './uphone.jpg'
 
 const Home = () => {
     return (
-        <div className='w-full flex justify-evenly items-center' style={{height: '80vh'}}>
+        <div className='bg-gradient-to-r from-black to-gray-800 text-white w-full flex justify-evenly items-center' style={{height: '80vh'}}>
             <article>
                 <h1 className='text-8xl'>uPhone 13 Pro</h1>
                 <p className='text-4xl my-2'>Oh.So.Pro</p>
-                <button className='bg-black py-2 px-5 my-5  text-white'>Buy Now</button>
+                <button className='bg-gray-700 py-2 px-5 my-5  text-white'>Buy Now</button>
             </article>
             <img src={Img} className='w-5/12' alt="" />
         </div>
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NotFoundImg  from './404.jpg'
 
 const NotFound = () => {
     return (
-        <div>
-            <img className='w-full h-screen' src="./404.jpg" alt=""/>
+        <div className='flex justify-center items-center' style={{height: '90vh'}}>
+            <img className='w-9/12' src={NotFoundImg} alt="Page Not Found "/>
         </div>
     );
 };
