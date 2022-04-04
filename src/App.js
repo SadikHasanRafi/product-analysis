8c80511e8130b53855c4a1bc7c1042623e9c3362
SadikHasanRafi <sadikkhasan@gmail.com>
2022-04-04 16:07:19 +0600

    dashboard and home page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Reviews from './Components/Reviews/Reviews'
 import Blogs from './Components/Blogs/Blogs'
-import Home from './Components/Home/Home.js'
+import Home from './Components/Home/Home'
 import Dashboard from './Components/Dashboard/Dashboard'
 import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header'
@@ -16,7 +16,7 @@ function App() {
         <Route path='/reviews' element={<Reviews/>}></Route>
         <Route path='/dashboard' element={<Dashboard/>}></Route>
         <Route path='/blogs' element={<Blogs/>}></Route>
-        <Route path='*' element={<NotFound/>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { CartesianGrid, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import Data from './data.json'
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>DashBoard</h1>
+        <div className='flex justify-center flex-col h-screen'>
+            <h1 className='text-center text-3xl'>DashBoard</h1>
+            <div className='flex justify-center items-center'>
+            <LineChart width={500} height={300} data={Data}>
+                <XAxis dataKey="investment"/>
+                <YAxis/>
+                <Tooltip/>
+                <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+                <Line type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }}/>
+            </LineChart>
+            <PieChart width={600} height={600}>
+                <Pie data={Data} dataKey="revenue" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                <Tooltip></Tooltip>
+                <Pie data={Data} dataKey="sell" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+            </PieChart>
+            </div>
         </div>
     );
 };
